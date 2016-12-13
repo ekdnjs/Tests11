ac609b041fe7849b3fd080766ee0280a303dd121
ekdnjs <ekdnjs9@naver.com>
2016-12-13 17:11:31 +0900

    test

--- models/Reserve.js
+++ models/Reserve.js
@@ -5,7 +5,7 @@ var schema = new Schema({
   useremail: {type:String, required:true, trim:true},
   hostemail: {type:String, required:true, trim:true},
   title: {type:String, required:true, trim:true, unique:true},
-  name: {type:String, required:true, trim:true}, // 예약자 이름
+  name: {type:String, required:true, trim:true}, 
   people: {type:Number, required:true}, 
   address: {type:String, required:true},
   content: {type:String, required:true},
